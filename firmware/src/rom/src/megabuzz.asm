738956b174b4f5c875770ccdf6a88bc5a21fc205
Andy Karpov <andykarpov@users.noreply.github.com>
2026-09-21 00:42:52 +0200

    Added real ports for megabuzz

--- firmware/src/rom/src/megabuzz.asm
+++ firmware/src/rom/src/megabuzz.asm
@@ -1,27 +1,76 @@
-        MODULE MegaBuzz
+    macro MB_SetCfgMode
+    push bc
+    ld a, MegaBuzz.REG_CTRL : ld bc, MegaBuzz.PORT_ZXUNO_REG : out (c), a
+    pop bc
+    endm
 
-; todo - rework to zxuno ports!
-PORT_DATA       EQU 0x7F    ; CFG port
-PORT_ROM_SWITCH EQU 0xBF    ; ROM select port
-ROM_SWITCH_VAL  EQU 0x01    ; ROM select port bit
+    macro MB_SetRomMode
+    push bc
+    ld a, MegaBuzz.REG_ROMBANK : ld bc, MegaBuzz.PORT_ZXUNO_REG : out (c), a
+    pop bc
+    endm
+
+    macro MB_SetCtrlMode
+    push bc
+    ld a, MegaBuzz.REG_CTRL : ld bc, MegaBuzz.PORT_ZXUNO_REG : out (c), a
+    pop bc
+    endm
+
+    macro MB_Send nn
+    push bc
+    ld a, nn : ld bc, MegaBuzz.PORT_ZXUNO_DATA : out (c), a
+    pop bc
+    endm
+
+    macro MB_SendA
+    push bc
+    ld bc, MegaBuzz.PORT_ZXUNO_DATA : out (c), a
+    pop bc
+    endm
+
+    macro MB_Read
+    push bc
+    ld bc, MegaBuzz.PORT_ZXUNO_DATA : in a, (c)
+    pop bc
+    endm
+
+    MODULE MegaBuzz
+
+PORT_ZXUNO_REG   EQU #FC3B     ; ZXUNO control port
+PORT_ZXUNO_DATA  EQU #FD3B     ; ZXUNO data port
+REG_CFG          EQU #F7       ; cfg byte r/w
+REG_ROMBANK      EQU #F8       ; ROM bank w (bit 0 = 0 - zx rom, 1 = cfg rom)
+REG_CTRL         EQU #F9       ; Control register w (bit 0 = 1 - reset trigger), r (bit 0 = 1 - flash busy)
 
 ;; Read current config
+;; OUT: A = cfg byte
 ReadConfig:
-        IN A, (PORT_DATA)
-        RET
+    MB_SetCfgMode
+    MB_Read
+    RET
 
 ;; Write config and switch to main ROM
+;; IN: A = cfg byte
 ApplyConfig:
-        OUT (PORT_DATA), A
+    MB_SetCfgMode
+    MB_SendA
+    MB_SetCtrlMode
+.wait_flash:
+    MB_Read
+    bit 0, a
+    jr nz, .wait_flash
+    ; continue to cancel action
 
-;; Cancel - switch back a normal ROM
+;; Cancel - switch back a normal ROM + soft reset trigger
 Cancel:
-        LD A, ROM_SWITCH_VAL
-        OUT (PORT_ROM_SWITCH), A
-        ; TODO: soft reset ?
+    MB_SetRomMode
+    MB_Send 0 ; switch to zx rom
+    MB_SetCtrlMode
+    MB_Send 1 ; send a reset trigger pulse
+    MB_Send 0
 
 .Infinite:
-        JR .Infinite
+    JR .Infinite
 
-        ENDMODULE
+    ENDMODULE
 
